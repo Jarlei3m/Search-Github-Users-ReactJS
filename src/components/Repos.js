45cecde5a88e04b462eae894474c3c63969859fc
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-27 18:08:04 -0300

    chore: Getting and counting languages from users repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,29 @@ import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+ 
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
 
+    //in case of language as 'null'
+    if(!language) return total; 
+
+    // check if the language property is already on the object
+    if(!total[language]) {
+      // if it´s not, create a new one as an object
+      total[language] = {label:language, value:1}
+    } else {
+      // if language is already on the object, keep the same instance and just add + 1 to the value
+      total[language] = {
+        ...total[language], 
+        value: total[language].value + 1
+      };
+    };
+
+    return total;
+  }, {});
+
+  
   const chartData = [
   {
     label: "HTML",
