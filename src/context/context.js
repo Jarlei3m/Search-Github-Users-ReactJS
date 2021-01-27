f04f0b4638f9f4c9f2fa42e41b7178397585bf8d
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-27 09:55:34 -0300

    chore: Context API

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,19 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+//give access to Provider - Github.Provider
+const GithubContext = React.createContext(); 
+
+const GithubProvider = ({children}) => {
+  
+  return (
+    <GithubContext.Provider value={'here'}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+// GithubProvider to wrap the whole applicaiton
+// GithubContext to have access to the props on the value
+export { GithubProvider, GithubContext };
