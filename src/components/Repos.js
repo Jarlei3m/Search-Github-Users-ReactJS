f79788ac0d1bf0d88f93d61ad63942db53741f80
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-28 10:36:30 -0300

    chore: Setup bar3d chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -70,8 +70,8 @@ const Repos = () => {
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
         <Column3D data={chartData} />
-        <Doughnut2D data={mostPopular}/>
-        <div></div>
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={chartData} />
 
       </Wrapper>
     </section>
