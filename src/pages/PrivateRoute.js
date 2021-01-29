e4a0d1d7e14f51f549d2aab315bb8da013434432
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-29 09:00:11 -0300

    feat: Setup private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = () => {
-  return <h2>private route component</h2>;
+// Private route will wrap the dashboard
+// need to have access to 'chidren' because the dashboard will be a child of 'PrivateRoute'
+// '...rest' collect the rest of the props that are in my component, whatever they are
+const PrivateRoute = ({ children, ...rest }) => {
+  const {isAuthenticate, user} = useAuth0()
+  const isUser = isAuthenticate && user;
+
+  return (
+    // spreading out the props on the Route {...rest}, placing in the Rout that i'm returning
+    <Route 
+      {...rest} 
+      render={() => {
+        return isUser ? children : <Redirect to="/login" />;
+      }}>
+
+    </Route>
+  );
 };
 export default PrivateRoute;
